a7e2b7408417d647b25980301d1ced64f66cb759
itdinaslh <itdlh@outlook.com>
2023-03-04 14:15:52 +0700

    Update Assets

---
+++ wwwroot/assets/js/pages/crud/forms/widgets/autosize.js
@@ -0,0 +1,31 @@
+// Class definition
+
+var KTAutosize = function () {
+
+    // Private functions
+    var demos = function () {
+        // basic demo
+        var demo1 = $('#kt_autosize_1');
+        var demo2 = $('#kt_autosize_2');
+        var demo3 = $('#kt_autosize_3');
+        var demo4 = $('#kt_autosize_4');
+
+        autosize(demo1);
+
+        autosize(demo2);
+        autosize.update(demo2);
+        autosize(demo3);
+        autosize(demo4);
+    }
+
+    return {
+        // public functions
+        init: function() {
+            demos();
+        }
+    };
+}();
+
+jQuery(document).ready(function() {
+    KTAutosize.init();
+});
