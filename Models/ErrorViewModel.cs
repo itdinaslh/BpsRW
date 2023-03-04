39c647f7fb195cefd88bbfcf59e6e452342e4e3e
itdinaslh <itdlh@outlook.com>
2023-03-04 14:11:08 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BpsRW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
